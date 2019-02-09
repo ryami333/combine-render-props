cf5ad30044f8e3f47254d576affe317e615c010a
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-09 15:21:24 +1300

    Add a 'creator' function.

--- src/App.js
+++ src/App.js
@@ -4,51 +4,57 @@ const FooContext = React.createContext('foo');
 const BarContext = React.createContext('bar');
 const BazContext = React.createContext('baz');
 
-function createAugmentedConsumer(Consumer) {
-  return class X extends React.Component {
-    renderChildren = (...consumerArgs) => {
-      const { args } = this.props;
-      return args
-        ? this.props.children(...consumerArgs, ...args)
-        : this.props.children(...consumerArgs);
-    };
-
-    render() {
-      return <Consumer>{this.renderChildren}</Consumer>;
-    }
-  };
-}
-
-const augmentedConsumers = [
-  FooContext.Consumer,
-  BarContext.Consumer,
-  BazContext.Consumer,
-].map(Consumer => createAugmentedConsumer(Consumer));
-
-const Combined = augmentedConsumers.reduce(
-  (Carry, Consumer) => {
+function combineConsumers(consumers) {
+  function createAugmentedConsumer(Consumer) {
     return class X extends React.Component {
-      static defaultProps = {
-        args: [],
-      };
-
-      renderCarry = (...carryArgs) => {
-        return this.props.children(...carryArgs);
-      };
-
-      renderConsumer = (...consumerArgs) => {
-        return <Carry args={[...consumerArgs]}>{this.renderCarry}</Carry>;
+      renderChildren = (...consumerArgs) => {
+        const { args } = this.props;
+        return args
+          ? this.props.children(...consumerArgs, ...args)
+          : this.props.children(...consumerArgs);
       };
 
       render() {
-        return (
-          <Consumer args={[...this.props.args]}>{this.renderConsumer}</Consumer>
-        );
+        return <Consumer>{this.renderChildren}</Consumer>;
       }
     };
-  },
-  ({ children, args }) => children(args),
-);
+  }
+
+  return consumers
+    .map(consumer => createAugmentedConsumer(consumer))
+    .reduce(
+      (Carry, Consumer) => {
+        return class X extends React.Component {
+          static defaultProps = {
+            args: [],
+          };
+
+          renderCarry = (...carryArgs) => {
+            return this.props.children(...carryArgs);
+          };
+
+          renderConsumer = (...consumerArgs) => {
+            return <Carry args={[...consumerArgs]}>{this.renderCarry}</Carry>;
+          };
+
+          render() {
+            return (
+              <Consumer args={[...this.props.args]}>
+                {this.renderConsumer}
+              </Consumer>
+            );
+          }
+        };
+      },
+      ({ children, args }) => children(args),
+    );
+}
+
+const Combined = combineConsumers([
+  FooContext.Consumer,
+  BarContext.Consumer,
+  BazContext.Consumer,
+]);
 
 function App() {
   return <Combined>{([foo, bar, baz]) => foo + bar + baz}</Combined>;
