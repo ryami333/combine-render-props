6594b3ec03638f8a15474b0222a067976dc63d2a
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-09 13:31:56 +1300

    Initial implementation with render-props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const FooContext = React.createContext('foo');
+const BarContext = React.createContext('bar');
+const BazContext = React.createContext('baz');
+
+const Combined = () => (
+    <FooContext.Consumer>
+        {foo => (
+            <BarContext.Consumer>
+                {bar => (
+                    <BazContext.Consumer>
+                        {baz => `${foo}${bar}${baz}`}
+                    </BazContext.Consumer>
+                )}
+            </BarContext.Consumer>
+        )}
+    </FooContext.Consumer>
+);
+
+function App() {
+    return <Combined />;
 }
 
 export default App;
