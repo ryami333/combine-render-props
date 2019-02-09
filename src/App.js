0ba2a316c1c09055c0e0fc295e6f53de2f3411c5
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-09 15:04:55 +1300

    Reinstante Combined using createAugmentedConsumer

--- src/App.js
+++ src/App.js
@@ -5,29 +5,45 @@ const BarContext = React.createContext('bar');
 const BazContext = React.createContext('baz');
 
 function createAugmentedConsumer(Consumer) {
-    return class X extends React.Component {
-        renderChildren = (...consumerArgs) => {
-            return this.props.children(...consumerArgs, this.props.args);
-        };
-
-        render() {
-            return <Consumer>{this.renderChildren}</Consumer>;
-        }
+  return class X extends React.Component {
+    renderChildren = (...consumerArgs) => {
+      const { args = [] } = this.props;
+      return this.props.children(...consumerArgs, ...args);
     };
+
+    render() {
+      return <Consumer>{this.renderChildren}</Consumer>;
+    }
+  };
 }
 
-const AugmentedFoo = createAugmentedConsumer(FooContext.Consumer);
-
 const augmentedConsumers = [
-    FooContext.Consumer,
-    BarContext.Consumer,
-    BazContext.Consumer,
+  FooContext.Consumer,
+  BarContext.Consumer,
+  BazContext.Consumer,
 ].map(Consumer => createAugmentedConsumer(Consumer));
 
+const Combined = augmentedConsumers.reduce(
+  (Carry, Consumer) => {
+    return ({ children, args }) => {
+      return (
+        <Consumer args={args}>
+          {(...consumerArgs) => {
+            return (
+              <Carry args={consumerArgs}>
+                {carryArgs => children(carryArgs)}
+              </Carry>
+            );
+          }}
+        </Consumer>
+      );
+    };
+  },
+  ({ children, args }) => children(args),
+);
+
 function App() {
-    return (
-        <AugmentedFoo args={['bar']}>{(foo, bar) => foo + bar}</AugmentedFoo>
-    );
+  return <Combined>{(...args) => args}</Combined>;
 }
 
 export default App;
