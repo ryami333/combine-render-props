cea2f9eaadb6810b7594b3d4aae499dfdd98e28f
Mitch Ryan <mitch.ryan@outlook.co.nz>
2019-02-09 14:28:49 +1300

    Try approach with AugmentedContext factory

--- src/App.js
+++ src/App.js
@@ -4,33 +4,24 @@ const FooContext = React.createContext('foo');
 const BarContext = React.createContext('bar');
 const BazContext = React.createContext('baz');
 
-const consumers = [
-    FooContext.Consumer,
-    BarContext.Consumer,
-    BazContext.Consumer,
-];
-
-const Combined = consumers.reduce(
-    (Carry, Consumer) => {
-        return class X extends React.Component {
-            renderChildren = (...args) =>
-                this.props.children(...this.carryArgs, ...args);
-
-            renderCarry = (...carryArgs) => {
-                this.carryArgs = carryArgs;
-                return <Consumer>{this.renderChildren}</Consumer>;
-            };
-
-            render() {
-                return <Carry>{this.renderCarry}</Carry>;
-            }
+function createAugmentedConsumer(Consumer) {
+    return class X extends React.Component {
+        renderChildren = (...consumerArgs) => {
+            return this.props.children(...consumerArgs, this.props.args);
         };
-    },
-    ({ children }) => children(),
-);
+
+        render() {
+            return <Consumer>{this.renderChildren}</Consumer>;
+        }
+    };
+}
+
+const AugmentedFoo = createAugmentedConsumer(FooContext.Consumer);
 
 function App() {
-    return <Combined>{(foo, bar, baz) => foo + bar + baz}</Combined>;
+    return (
+        <AugmentedFoo args={['bar']}>{(foo, bar) => foo + bar}</AugmentedFoo>
+    );
 }
 
 export default App;
